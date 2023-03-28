47b63954ce5ad8d41462f11b538d3a63c63d1010
treppenwitz03 <owenlegrande@gmail.com>
2023-03-28 10:53:37 +0800

    HAhaha

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from PyQt5.QtWidgets import QApplication
-from Seiri.Window import Window
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import *
+from Seiri.Window import Ui_MainWindow
 import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = Window()
+    w = QMainWindow()
+    w.setWindowFlag(Qt.WindowType.MSWindowsFixedSizeDialogHint)
+    wu = Ui_MainWindow()
+    wu.setupUi(w)
     w.show()
     
     app.exec()
